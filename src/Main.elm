1f2adf2a03064782b71875a35acd6dee61d94551
Ralf Northman <ralf.northman@bredband.net>
2019-05-12 06:08:39 +0200

    Initial commit.

---
+++ src/Main.elm
@@ -0,0 +1,132 @@
+module Main exposing (main)
+
+import Color
+import Color.Generator as ColGen
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Html exposing (Html)
+import Palette.Tango as Tango
+
+
+col : Color.Color -> Color
+col paletteColor =
+    paletteColor
+        |> Color.toRGB
+        |> (\( red, green, blue ) ->
+                rgb255 (round red) (round green) (round blue)
+           )
+
+
+googleFont : String -> Attribute msg
+googleFont fontName =
+    let
+        fontString =
+            String.replace " " "+" fontName
+    in
+    Font.family
+        [ Font.external
+            { url =
+                "https://fonts.googleapis.com/css?family="
+                    ++ fontString
+            , name = fontName
+            }
+        ]
+
+
+display =
+    googleFont "Pinyon Script"
+
+
+montserrat =
+    googleFont "Montserrat"
+
+
+scale =
+    modular 64 2.0
+        >> round
+
+
+color =
+    let
+        base =
+            Tango.scarletRed1
+
+        ( comp1, comp2 ) =
+            ColGen.splitComplementary 150 base
+
+        background =
+            comp2 |> ColGen.shade 40 |> ColGen.tone -20
+
+        engine =
+            comp1 |> ColGen.tint 10 |> ColGen.tone -55
+
+        design =
+            base |> ColGen.tint 5 |> ColGen.tone -35
+    in
+    { design = col design, background = col background, engine = col engine }
+
+
+type Visibility
+    = Visible
+    | Ghost
+
+
+designText : Visibility -> Element msg
+designText vis =
+    let
+        diff =
+            case vis of
+                Visible ->
+                    [ Font.color color.design
+                    , Font.shadow { offset = ( 1, 1 ), blur = 2, color = rgb 0 0 0 }
+                    ]
+
+                Ghost ->
+                    [ Font.color color.background ]
+    in
+    el
+        (diff
+            ++ [ display
+               , Font.size <| scale 2
+               , centerX
+               , moveDown 12
+               ]
+        )
+    <|
+        text "Design"
+
+
+engineeringText : Element msg
+engineeringText =
+    el
+        [ montserrat
+        , Font.size <| scale 1
+        , Font.hairline
+        , Font.letterSpacing -1
+        , centerX
+        , paddingXY 8 16
+        , Background.color color.engine
+        , above <| designText Visible
+        , Border.shadow { offset = ( 2, 2 ), size = 3, blur = 5, color = rgb 0 0 0 }
+        ]
+    <|
+        text "Engineering"
+
+
+main : Html msg
+main =
+    layout
+        [ padding 20
+        , Background.color color.background
+        ]
+    <|
+        column
+            [ spacing 0
+            , width fill
+            , height fill
+            ]
+            [ designText Ghost
+            , engineeringText
+            ]
